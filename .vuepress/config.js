daae2e883026e2604fb00a1e9e6741f540bb989e
darbyjack <admin@glaremasters.me>
2019-06-15 19:35:55 -0500

    Fixed background color

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -54,6 +54,7 @@ module.exports = {
           children: [
             '/project/introduction.md',
             '/project/about.md',
+            '/project/changelog.md',
           ]
         },
         {
